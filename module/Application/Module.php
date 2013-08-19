099b32626448a1526d1b6911d2e4f29141b51dd8
Jan Esser <esserj@gmail.com>
2013-08-19 08:46:16 +0200

    applied interfaces

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -9,12 +9,27 @@
 
 namespace Application;
 
+use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
+use Zend\ModuleManager\Feature\InitProviderInterface;
+use Zend\ModuleManager\ModuleEvent;
+use Zend\ModuleManager\ModuleManagerInterface;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 
 class Module
+    implements  BootstrapListenerInterface,
+                ConfigProviderInterface,
+                AutoloaderProviderInterface
 {
-    public function onBootstrap(MvcEvent $e)
+    /**
+     * @param MvcEvent|EventInterface $e
+     * @return array|void
+     */
+    public function onBootstrap(EventInterface $e)
     {
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
